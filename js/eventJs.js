712d2bcbd8142c32bb05ee0e48bb46fc753d57b5
WangApple <aa8821266@gmail.com>
2016-11-09 22:58:22 +0800

    Add files via upload

--- js/eventJs.js
+++ js/eventJs.js
@@ -1,5 +1,5 @@
 $(function(){
-	$('#editDiv').dialog({
+	$('#insertDiv').dialog({
 		position: { 
 			my: 'center',
 			at: 'center',
@@ -12,40 +12,82 @@ $(function(){
 				}
 			}
 		},
-		buttons:{
+		/*buttons:{
 			'提交': function(){
 				alert('正在提交Ajax呦...');
+				$(this).dialog('insert');
 			},
 			'取消': function(){
 				$(this).dialog('close');
 			}
 				
 			
-		},
+		},*/
 
 		
-		beforeClose:function(e,ui){
+		/*beforeClose:function(e,ui){
 			alert('確定要關閉嗎?');
-		},
+		},*/
 
 		maxWidth:350,
 		minWidth:330,
-		maxHeight:420,
-		minHeight:400,
+		maxHeight:400,
+		minHeight:330,
 		autoOpen:false,
         modal: true,
         resizable: true,
         closeOnEscape: true,
         show:true,
 		hide:true,
-        title: "編輯事件",
+        title: "新增事件",
 		closeText: "X"
 	});
 
 
 
-	$('#event_div #event_footer #edit_button').click(function(){
-		$('#editDiv').dialog('open');
+	$('#event_div #event_footer #insert_button').click(function(){
+		$('#insertDiv').dialog('open');
+	});
+
+
+
+	$('#deleteDiv').dialog({
+		position: { 
+			my: 'center',
+			at: 'center',
+			collision: 'fit',
+			// ensure that the titlebar is never outside the document
+			using: function(pos) {
+				var topOffset = $(this).css(pos).offset().top;
+				if (topOffset < 0) {
+					$(this).css('top', pos.top - topOffset);
+				}
+			}
+		},
+		
+
+		maxWidth:350,
+		minWidth:330,
+		maxHeight:250,
+		minHeight:230,
+		autoOpen:false,
+        modal: true,
+        resizable: true,
+        closeOnEscape: true,
+        show:true,
+		hide:true,
+        title: "刪除事件",
+		closeText: "X"
+	});
+
+
+
+	$('#event_div #event_footer #delete_button').click(function(){
+		$('#deleteDiv').dialog('open');
 	});
 
 });
+
+
+//document.write('<script src="event.html"></script>');
+
