5340e1f2e76be84c824c2559ad2973bc21e754f0
WangApple <aa8821266@gmail.com>
2016-11-21 21:31:58 +0800

    Add files via upload

---
+++ loginJs.js
@@ -0,0 +1,14 @@
+$(function(){
+	$(window).load(function(){
+        $('.loading').fadeOut(2500);
+
+    });
+
+
+	setTimeout(function(){
+		$('#initiateDiv').show(1000);
+		$('#welcomeDiv').show(1000);
+	},3200);
+
+
+});
