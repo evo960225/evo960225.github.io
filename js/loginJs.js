e285ffe8f8a2add2e28e7259ce842876d6715cb4
WangApple <aa8821266@gmail.com>
2016-11-22 22:48:55 +0800

    Add files via upload

--- js/loginJs.js
+++ js/loginJs.js
@@ -1,14 +1,45 @@
 $(function(){
-	$(window).load(function(){
-        $('.loading').fadeOut(2500);
+    $('.loading').fadeOut(2500);
+    setTimeout(function(){
+	$('#initiateDiv').show(1000);
+	$('#welcomeDiv').show(1000);
+	},3200);
 
-    });
 
+	$('#infoDiv').dialog({
+		position: { 
+			my: 'center',
+			at: 'center',
+			collision: 'fit',
+			// ensure that the titlebar is never outside the document
+			using: function(pos) {
+				var topOffset = $(this).css(pos).offset().top;
+				if (topOffset < 0) {
+					$(this).css('top', pos.top - topOffset);
+				}
+			}
+		},
+		
 
-	setTimeout(function(){
-		$('#initiateDiv').show(1000);
-		$('#welcomeDiv').show(1000);
-	},3000);
+		maxWidth:460,
+		minWidth:400,
+		maxHeight:400,
+		minHeight:300,
+		autoOpen:false,
+        modal: true,
+        resizable: false,
+        closeOnEscape: true,
+        show:true,
+		hide:true,
+		closeText: "X",
+		background:'yellow'
 
+	});
+
+	$('#info').click(function(){
+		$('#infoDiv').dialog('open');
+	});
 
 });
+
+
